0c0ee65a97ad7b461931cdfe4be4bc6b1b23f29a
Carlos Vargas <varlopecar@gmail.com>
2025-04-03 10:29:45 +0200

    chore: add tailwind and gsap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://varlopecar.com',
-	integrations: [mdx(), sitemap()],
-	output: 'static',
+  site: 'https://varlopecar.com',
+  integrations: [mdx(), sitemap()],
+  output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
